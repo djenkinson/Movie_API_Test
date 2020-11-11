7d809eaf3ba116056aba18b545ccbb7991652e76
Dan Jenkinson <d.m.jenkinson@gmail.com>
2020-11-11 15:21:24 +0000

    Setup code files and added test data.

---
+++ Movie_API/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_API.Models
+{
+    public class Movie
+    {
+        public string movieId { get; set; }
+        public string title { get; set; }
+        public string language { get; set; }
+        public string duration { get; set; }
+        public int releaseYear { get; set; }
+    }
+}
---
+++ Movie_API/Models/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_API.Models
+{
+    public class Stats
+    {
+        public string movieId { get; set; }
+        public int watchDurationMs { get; set; }
+    }
+}
