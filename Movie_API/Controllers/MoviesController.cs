0a2d0c78ff4ba7e07b6aa0d4348e7cfae7485b86
Dan Jenkinson <d.m.jenkinson@gmail.com>
2020-11-11 17:27:07 +0000

    Added Movies controller

---
+++ Movie_API/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Movie_API.Data;
+using Movie_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/stats")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ILogger<MoviesController> _logger;
+        private DataManager dm = new DataManager();
+
+        public MoviesController(ILogger<MoviesController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet()]
+        public List<MovieStats> Get(int movieid = 1)
+        {
+            List<MovieStats> movieStatsList = dm.getMovieStats();
+
+            return movieStatsList;
+        }
+    }
+}
